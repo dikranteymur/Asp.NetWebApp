8e9518844b7252dc32a3f0b663032fe3d2308354
dikranteymur <mdteymur@gmail.com>
2021-03-21 00:10:50 +0300

    Yeni bir controller eklendi - UrunIslemleriController, yeni bir view eklendi - UrunIslemleri, controller'da 'urunler' isminde yeni bir 'List<string> urunler' verisi eklendi ve 'x' degiskeni ile view'e gonderildi. View'de bu degiskeni kullanmak icin Razor Goruntu Motoru kullanildi.

---
+++ Controllers/UrunIslemleriController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ViewYazmak.Controllers
+{
+    public class UrunIslemleriController : Controller
+    {
+        List<string> urunler = new List<string>  // sanal veritabani
+        {
+            "kalem",
+            "silgi",
+            "defter",
+            "kitap",
+            "kalemtras",
+            "cikolata"
+        };
+
+        // IDE'ye mvc yaparsak mvc ile alakali snippet'ler gelir
+        public IActionResult Index()  // listeleme islemleri Index ile yapilir (CRUD ---> R)
+        {
+            // ViewBag dinamik oldugu icin x yerine istedigimiz bir isim verebiliriz.
+            ViewBag.x = urunler;
+            return View();
+        }
+
+    }
+}
